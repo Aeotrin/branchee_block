cc82c23c46e2051ad2a46277e58c815654c8bfd6
Dan Surkan <dan.surkan@zu.com>
2015-12-28 12:53:47 -0600

    Moved theme template for branchee menu from block template override to module template. Added themeing options

--- TODO.md
+++ TODO.md
@@ -1,4 +1,2 @@
-* add site menu's to the menu select list
-* add default themes or custom (and textfield)
 * add branchee js initialization to the module
 * include branchee library in module and give instructions for adding it to the sites library file
--- src/Plugin/Block/BrancheeMenuBlock.php
+++ src/Plugin/Block/BrancheeMenuBlock.php
@@ -41,15 +41,13 @@ class BrancheeMenuBlock extends BlockBase implements BlockPluginInterface {
       $tree = $menu_tree->transform($tree, $manipulators);
       $tree = $menu_tree->build($tree);
 
-      return $tree;
+      $theme = $config['theme'] == 'other' ? $config['theme_other'] : $config['theme'];
+      return [
+        '#theme' => 'branchee_menu_block',
+        '#branchee_theme' => $theme,
+        '#menu' => $tree,
+      ];
     }
-
-    return array(
-      '#markup' => $this->t('Hello @menu!', array (
-          '@menu' => $menu,
-        )
-      ),
-    );
   }
 
   /**
@@ -60,18 +58,47 @@ class BrancheeMenuBlock extends BlockBase implements BlockPluginInterface {
 
     $config = $this->getConfiguration();
     $menus = entity_load_multiple('menu');
-    $options = array();
+    $menu_options = [];
     foreach ($menus as $key => $menu) {
-      $options[$menu->get('id')] = $menu->get('label');
+      $menu_options[$menu->get('id')] = $menu->get('label');
     }
 
-    $form['branchee_menu_block_menu'] = array (
+    $form['branchee_menu_block_menu'] = [
       '#type' => 'select',
       '#title' => $this->t('Select a Menu'),
       '#description' => $this->t('Select a Menu to render with Branchee'),
       '#default_value' => isset($config['menu']) ? $config['menu'] : 'main',
-      '#options' => $options,
-    );
+      '#options' => $menu_options,
+    ];
+
+    $theme_options = [
+      'base' => 'Base',
+      'minimal' => 'Minimal',
+      'rainbow' => 'Rainbow',
+      'dark-rainbow' => 'Dark Rainbow',
+      'deep-blue' => 'Deep Blue',
+      'other' => 'Other',
+    ];
+
+    $form['branchee_menu_block_theme'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Select a Theme'),
+      '#description' => $this->t('Select a Theme for the branchee menu, or a custom class to apply to it.'),
+      '#default_value' => isset($config['theme']) ? $config['theme'] : 'base',
+      '#options' => $theme_options,
+    ];
+
+    $form['branchee_menu_block_theme_other'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Other'),
+      '#description' => $this->t('Type in a custom theme name to use.'),
+      '#default_value' => isset($config['theme_other']) ? $config['theme_other'] : '',
+      '#states' => array(
+        'visible' => array(
+          ':input[name="settings[branchee_menu_block_theme]"]' => array('value' => 'other'),
+        ),
+      ),
+    ];
 
     return $form;
   }
@@ -81,5 +108,7 @@ class BrancheeMenuBlock extends BlockBase implements BlockPluginInterface {
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->setConfigurationValue('menu', $form_state->getValue('branchee_menu_block_menu'));
+    $this->setConfigurationValue('theme', $form_state->getValue('branchee_menu_block_theme'));
+    $this->setConfigurationValue('theme_other', $form_state->getValue('branchee_menu_block_theme_other'));
   }
 }
